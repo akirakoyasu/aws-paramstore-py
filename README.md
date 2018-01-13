6c121c9d218c9ed4c130730c9a0c70f220e5a7e5
Akira Koyasu <mail@akirakoyasu.net>
2018-01-13 19:35:17 +0900

    add: package (#1)

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # aws-paramstore-py
+
+Query params from AWS System Manager Parameter Store
+
+## Install
+
+```bash
+pip install aws-paramstore-py
+```
+
+## Usage
+
+in console
+```bash
+# AWS credentials from env vars
+aws-pspy /path/to/params
+# returns {"key1": "value1", "key2": "value2"}
+```
+
+in Python
+```python
+import aws_paramstore_py as paramstore
+
+# AWS credentials from env vars
+params = paramstore.get('/path/to/params')
+# dict(key1: "value1", key2: "value2")
+```
---
+++ aws_paramstore_py/__init__.py
@@ -0,0 +1,38 @@
+import argparse
+import json
+
+import boto3
+
+__version__ = '0.0.1'
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Query params from AWS System Manager Parameter Store')
+    parser.add_argument('paths', metavar='path', nargs='*', help='The hierarchy for the parameter')
+    parser.add_argument('--decryption', action='store_true', help='Decrypt secure values or not')
+    args = parser.parse_args()
+    paths = args.paths
+    decryption = args.decryption
+    params = get(*paths, decryption=decryption)
+    print(json.dumps(params))
+
+
+def get(*paths, decryption=False):
+    ssm = boto3.client('ssm')
+    path = '/'.join(paths)
+    if path[:1] != '/':
+        path = '/' + path
+    if path[len(path):] != '/':
+        path = path + '/'
+    response = ssm.get_parameters_by_path(Path=path, Recursive=True, WithDecryption=decryption)
+    params = map(lambda p: _remove_prefix(p, path), response['Parameters'])
+    return _convert_to_dict(params)
+
+
+def _remove_prefix(param, prefix):
+    param['Name'] = param['Name'][len(prefix):]
+    return param
+
+
+def _convert_to_dict(params):
+    return {p['Name']: p['Value'] for p in params}
