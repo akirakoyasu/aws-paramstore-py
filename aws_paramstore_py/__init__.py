060d1f95f8326298ca63b66f8e88ea3174b46655
Akira Koyasu <mail@akirakoyasu.net>
2018-01-22 00:05:43 +0900

    add: bash export

--- aws_paramstore_py/__init__.py
+++ aws_paramstore_py/__init__.py
@@ -1,18 +1,28 @@
 import argparse
 import json
+import shlex
 
 from .main import get
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 
 def cli():
+    def print_bash_export(params):
+        for key, value in params.items():
+            var_name = key.replace('/', '_').replace('-', '_').replace('.', '_')
+            var_val = shlex.quote(value)
+            print('export {0}={1}'.format(var_name, var_val))
+
     parser = argparse.ArgumentParser(description='Query params from AWS System Manager Parameter Store')
     parser.add_argument('paths', metavar='path', nargs='*', help='The hierarchy for the parameter')
     parser.add_argument('--decryption', action='store_true', help='Decrypt secure values or not')
+    parser.add_argument('--bash-export', action='store_true', help='print bash export script')
     parser.add_argument('--version', action='version', version=__version__)
     args = parser.parse_args()
-    paths = args.paths
-    decryption = args.decryption
-    params = get(*paths, decryption=decryption)
-    print(json.dumps(params))
+
+    params = get(*args.paths, decryption=args.decryption)
+    if args.bash_export:
+        print_bash_export(params)
+    else:
+        print(json.dumps(params))
